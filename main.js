405092c2440cbecea33489b6413abdc4a817a66b
Christian Guizar <guizar_chris@yahoo.com>
2022-09-04 16:05:06 -0500

    Daily Codewars Push 7Kyu Factorial

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
-// Example(Input1, Input2 --> Output)
+// Your task is to write function factorial.
 
-// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
-
-function divisibleBy(numbers, divisor){
-  return numbers.filter(x => x % divisor === 0)
+function factorial(n){
+  let res = 1;
+  for(let i = n; i > 0; i--){
+    res *= i
+  }
+  return res
 }
 
+// const factorial = n => n ? factorial(n - 1) * n : 1;
+
+// function factorial(n){
+//   let answer = 1;
+  
+//   while (n > 0) {
+//     answer *= n;
+//     n--;
+//   }
+  
+//   return answer;
+// }
