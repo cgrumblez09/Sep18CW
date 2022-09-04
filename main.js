e98a03e14f8f98c1be0d857b6ac9774ffeed185f
Christian Guizar <guizar_chris@yahoo.com>
2022-09-04 16:14:26 -0500

    8kyu CW Push

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-// Your task is to write function factorial.
+// This function should test if the factor is a factor of base.
 
-function factorial(n){
-  let res = 1;
-  for(let i = n; i > 0; i--){
-    res *= i
-  }
-  return res
+// Return true if it is a factor or false if it is not.
+// About factors
+
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+//     You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+//     You can use the mod operator (%) in most languages to check for a remainder
+
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
+
+
+function checkForFactor (base, factor) {
+  return base % factor == 0
 }
-
-// const factorial = n => n ? factorial(n - 1) * n : 1;
-
-// function factorial(n){
-//   let answer = 1;
-  
-//   while (n > 0) {
-//     answer *= n;
-//     n--;
-//   }
-  
-//   return answer;
-// }
