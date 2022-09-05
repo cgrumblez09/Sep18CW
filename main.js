38e352f86693de927d9246a2188e056885fc5207
Christian Guizar <guizar_chris@yahoo.com>
2022-09-05 13:03:58 -0500

    Daily Codewars Push 7Kyu arrays and methods

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-// This function should test if the factor is a factor of base.
+// Description:
 
-// Return true if it is a factor or false if it is not.
-// About factors
+// Given the triangle of consecutive odd numbers:
 
-// Factors are numbers you can multiply together to get another number.
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
 
-// 2 and 3 are factors of 6 because: 2 * 3 = 6
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 
-//     You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
-//     You can use the mod operator (%) in most languages to check for a remainder
+// 1 -->  1
+// 2 --> 3 + 5 = 8
 
-// For example 2 is not a factor of 7 because: 7 % 2 = 1
-
-// Note: base is a non-negative number, factor is a positive number.
-
-
-function checkForFactor (base, factor) {
-  return base % factor == 0
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
 }
+
+// function rowSumOddNumbers(n) {
+//   var start = n * n - n + 1;
+//   var result = 0;  
+  
+//   for(i = 0; i < n; i++)
+//   {
+//     result =  result + (start + (i*2));
+//   }
+    
+//    return result;
+// }
+
+// function rowSumOddNumbers(n) {
+//   var first = (n*(n-1)) + 1;
+//   var result = 0;
+//   for (i = 0; i < n; i++) {
+//     result += first;
+//     first += 2;
+//   }
+//   return result;
+// }
