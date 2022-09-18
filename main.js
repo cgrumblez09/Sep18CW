6672e3674905c5318ce43e0d774c98e632afcd31
Christian Guizar <guizar_chris@yahoo.com>
2022-09-18 16:35:55 -0500

    Daily CW Push String Methods Arrays

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
-function removeChar(str){
-  let start = str.split('').splice(1)
-  console.log(start)
-  let end = start.splice(-1, 1)
-  return start.join('')  
-};
+//     You are to write a function that takes a string as it's first paramter. This string will be a string of words.
 
-// function removeChar(str) {
-//   return str.slice(1, -1);
-// }
-// function removeChar(str){
-//   //You got this!
-//    str1 = str.split('');
-//    str1.shift();
-//    str1.pop();
-//    return str1.join('');
-     
-//  };
- 
- 
+//     You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+
+//     Once you have the located string you are finally going to multiply by it the third provided paramater, which will also be an interger. You are additionally required to add a hyphen in between each word.
+
+// Example
+
+// modifyMultiply ("This is a string",3,5) 
+
+// Should return
+
+// "string-string-string-string-string"
+
+// Since the 3rd word is 'string'(starting from 0 remember) and the third paramater indicates that it should be repeated 5 times.
+
+// Simple. Good luck.
+
+function modifyMultiply(str,i,n) {
+  return Array(n).fill(str.split(' ')[i]).join('-');
+} 
+
+// const modifyMultiply = (s,loc,num) => new Array(num).fill(s.split(' ')[loc]).join('-')
+
+// function modifyMultiply (str,loc,num) {
+//   let arr = str.split(' ')
+//   let res = []
+//   let strres =  ""
+//   for (i =0; i<num; i++){
+//    res.push(arr[loc]);
+//    strres = res.join('-');
+//   }
+//   return strres
+// } 
+
